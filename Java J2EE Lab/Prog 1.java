6401d9b62ddb65ace6551ee2db175cb8c61a8a92
ankit-k-47 <162859636+ankit-k-47@users.noreply.github.com>
2024-03-11 00:03:23 +0530

    Update Prog 1.java

--- Java J2EE Lab/Prog 1.java
+++ Java J2EE Lab/Prog 1.java
@@ -1,83 +1,58 @@
-// Contact class
-
-public class Contact {
-String name;
-Long number;
-Contact(){}
-Contact(String name, Long number) {
-this.name = name;
-this.number = number;
-	}
-}
-
-
-
-// Main class
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+
 public class Main {
-	
-static void print(Contact c1) {
-System.out.println("Name : " + c1.name);
-System.out.println("Number : " + c1.number);
-}
+    static void print(Contact c) {
+        System.out.println("Name : " + c.getName());
+        System.out.println("Number : " + c.getNumber());
+    }
 
-public static void main(String[] args) {
-	
-ArrayList<Contact> contactList = new ArrayList<Contact>();
-int numberofContacts;
-System.out.println("Enter the number of contacts to be saved : ");
-Scanner sc = new Scanner(System.in);
-numberofContacts = sc.nextInt();
-for(int i=0; i<numberofContacts; i++) {
-String cname;
-Long cnumber;
-System.out.println("Enter the name of the person : ");
-sc.nextLine();
-cname = sc.nextLine();
-System.out.println("Enter the number of the person : ");
-cnumber = sc.nextLong();
-Contact c1 = new Contact(cname, cnumber);
-contactList.add(c1);
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Contact> contactList = new ArrayList<>();
+        
+        System.out.println("Enter the number of contacts to be saved : ");
+        int numberOfContacts = sc.nextInt();
+        sc.nextLine(); // consume newline
+        for (int i = 0; i < numberOfContacts; i++) {
+            System.out.println("Enter the name of the person : ");
+            String name = sc.nextLine();
+            System.out.println("Enter the number of the person : ");
+            long number = sc.nextLong();
+            sc.nextLine(); // consume newline
+            Contact c1 = new Contact(name, number);
+            contactList.add(c1);
+        }
 
-for(Contact c : contactList) {
-print(c);
-}
+        for (Contact c : contactList) {
+            print(c);
+        }
 
-int numberofMissedCalls;
-System.out.println("Enter the number of missed calls ");
-numberofMissedCalls = sc.nextInt();
-HashMap<Long, String> missedCalls = new HashMap<Long, String>();
-for(int i=0; i<numberofMissedCalls; i++) {
-Long missednumber;
-System.out.println("Enter the calling number : ");
-missednumber = sc.nextLong();
-String name="Private Caller";
-for(Contact c : contactList) {
-if(c.number.equals(missednumber)) {
-name = c.name;
-break;
-}
-}
-missedCalls.put(missednumber, name);
-}
-missedCalls.forEach((number,name) -> System.out.println("Number : " + number + " Name: " + name));
-Long delNumber;
-System.out.println("Enter the number to delete : ");
-delNumber = sc.nextLong();
-Contact delContact = new Contact();
-for(Contact c : contactList) {
-if(c.number.equals(delNumber)) {
-delContact = c;
-break;
-}
-}
-contactList.remove(delContact);
-for(Contact c : contactList) {
-print(c);
-}
-}
+        System.out.println("Enter the number of missed calls: ");
+        int numberOfMissedCalls = sc.nextInt();
+        HashMap<Long, String> missedCalls = new HashMap<>();
+        for (int i = 0; i < numberOfMissedCalls; i++) {
+            System.out.println("Enter the calling number : ");
+            long missedNumber = sc.nextLong();
+            String name = "Private Caller";
+            for (Contact c : contactList) {
+                if (c.getNumber().equals(missedNumber)) {
+                    name = c.getName();
+                    break;
+                }
+            }
+            missedCalls.put(missedNumber, name);
+        }
+        missedCalls.forEach((number, name) -> System.out.println("Number : " + number + " Name: " + name));
+
+        System.out.println("Enter the number to delete : ");
+        long delNumber = sc.nextLong();
+        contactList.removeIf(c -> c.getNumber().equals(delNumber));
+        for (Contact c : contactList) {
+            print(c);
+        }
+
+        sc.close(); // Closing scanner
+    }
 }
